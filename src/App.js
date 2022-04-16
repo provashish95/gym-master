3d1df1321fff40c64e012a17ab26ff7641629d6d
Provashish Roy <provashishroy95@gmail.com>
2022-04-17 00:53:32 +0600

    added custom link on hedaer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
-
-
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/services' element={<p>This is services</p>}></Route>
+        <Route path='/blogs' element={<p>This is Blogs</p>}></Route>
+        <Route path='*' element={<p>Not found</p>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#">Navbar</a>
+                {/* <CustomLink className="navbar-brand" to="/"></CustomLink> */}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Home</a>
+                            <CustomLink className="nav-link" aria-current="page" to="/">Home</CustomLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Blogs</a>
+                            <CustomLink className="nav-link" to="/services">Services</CustomLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Services</a>
+                            <CustomLink className="nav-link" to="/blogs">Blogs</CustomLink>
                         </li>
                     </ul>
                 </div>
