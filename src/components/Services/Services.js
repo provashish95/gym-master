dd6d8714e7118d28211645b526bfc3224e8b6715
Provashish Roy <provashishroy95@gmail.com>
2022-04-18 14:42:20 +0600

    updated

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Service = ({ service }) => {
+const Services = ({ service }) => {
     const navigate = useNavigate();
     const { name, price, description, img } = service;
 
@@ -20,4 +20,4 @@ const Service = ({ service }) => {
     );
 };
 
-export default Service;
+export default Services;
