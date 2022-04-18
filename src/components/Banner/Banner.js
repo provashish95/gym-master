890eb71062e5bdb4b29ab75b7c569caa1ce6d785
Provashish Roy <provashishroy95@gmail.com>
2022-04-18 14:23:18 +0600

    added font family

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,6 +3,7 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../images/banner/banner-1.jpg';
 import banner2 from '../../images/banner/banner-2.jpg';
 import banner3 from '../../images/banner/banner-3.jpg';
+import './Banner.css';
 
 const Banner = () => {
     const [index, setIndex] = useState(0);
@@ -19,8 +20,8 @@ const Banner = () => {
                     alt="FirstSlide"
                 />
                 <Carousel.Caption>
-                    <h3 className='text-dark'>Be Happy With Healthy Life</h3>
-                    <p className='text-dark'> Structured activity designed to promote health in human beings.</p>
+                    <h3 >Be Happy With Healthy Life</h3>
+                    <p>Fitness is not a destination. It is a way of life</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -31,8 +32,8 @@ const Banner = () => {
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>Don't Give Up</h3>
+                    <p>Make yourself stronger than your excuses.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -43,9 +44,9 @@ const Banner = () => {
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
+                    <h3>Make yourself proud</h3>
                     <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        Push harder than yesterday if you want a different tomorrow
                     </p>
                 </Carousel.Caption>
             </Carousel.Item>
